808868a25a03521aeb48a89bd59fec889eabc706
AleksTolstik <oleksii.tolstik@nure.ua>
2024-12-22 00:41:22 +0200

    Update login.controller.js

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -40,4 +40,22 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/getOrganization/:providerId', async (req, res) => {
+    const { providerId } = req.params;
+
+    try {
+        const query = `SELECT "company_name" FROM "Provider" WHERE "provider_id" = $1`;
+        const result = await req.db.query(query, [providerId]);
+
+        if (result.rows.length > 0) {
+            res.status(200).json({ organizationName: result.rows[0].organization_name });
+        } else {
+            res.status(404).json({ message: 'Provider not found' });
+        }
+    } catch (error) {
+        console.error('Error fetching organization:', error.message);
+        res.status(500).json({ message: 'Failed to fetch organization', error: error.message });
+    }
+});
+
 export const loginRouter = router;
