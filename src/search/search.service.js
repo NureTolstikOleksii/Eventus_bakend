94c2e367ade8d6b69c99c6570e7d6fd281b07d33
AleksTolstik <oleksii.tolstik@nure.ua>
2024-12-21 18:49:56 +0200

    Update search.service.js

--- src/search/search.service.js
+++ src/search/search.service.js
@@ -1,21 +1,25 @@
 export class SearchService {
-  // Метод для поиска услуг по ключевому слову
-  async searchServices(db, keyword) {
-      try {
-          const sqlQuery = `
-              SELECT * 
-              FROM "Service" 
-              WHERE "name" ILIKE $1 OR "description" ILIKE $2
-          `;
-          const paramsArray = [`%${keyword}%`, `%${keyword}%`];
-
-          const result = await db.query(sqlQuery, paramsArray);
-
-          return result.rows;
-      } catch (error) {
-          throw new Error('Error searching services: ' + error.message);
-      }
+    // Метод для поиска услуг по ключевому слову с изображениями
+    async searchServices(db, keyword) {
+        try {
+            const sqlQuery = `
+                SELECT 
+                    s.*,
+                    p."photo_url"
+                FROM "Service" s
+                LEFT JOIN "Photo" p ON s."service_id" = p."service_id"
+                WHERE s."name" ILIKE $1 OR s."description" ILIKE $2
+            `;
+            const paramsArray = [`%${keyword}%`, `%${keyword}%`];
+  
+            const result = await db.query(sqlQuery, paramsArray);
+  
+            return result.rows;
+        } catch (error) {
+            throw new Error('Error searching services: ' + error.message);
+        }
+    }
   }
-}
-
-export default new SearchService();
+  
+  export default new SearchService();
+  
